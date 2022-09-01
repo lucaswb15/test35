a9c783e58f015f514e3e4a0f0079cdf659e8f3e7
Milk-Equity <93746647+Milk-Equity@users.noreply.github.com>
2022-09-01 16:56:53 -0400

    video

---
+++ components/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Main = () => {
+  return (
+    <div >
+        <video muted autoPlay loop src={(require('/public/milk1.mp4'))} type='video/mp4'/>
+    </div>
+  );
+};
+
+
+export default Main
