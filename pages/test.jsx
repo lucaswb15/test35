22fd52a8af3f5d2ad3c6d8dfedfed85496691a04
Milk-Equity <93746647+Milk-Equity@users.noreply.github.com>
2022-09-02 20:55:24 -0400

    video

--- pages/test.jsx
+++ pages/test.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { motion } from 'framer-motion';
 
-const Main = () => {
+const test = () => {
   return (
     <div className='relative flex h-screen overflow-hidden'>
-      <motion.div animate={{x:[-300,0]}} className='flex z-20 p-5 items-center md:pl-32 justify-center'>
+      <motion.div animate={{x:[-200,0]}} className='flex z-20 p-5 items-center md:pl-48 justify-center'>
         <h1 className='text-7xl text-white font-bold'>Milk Equity</h1>
         </motion.div>
         <div className='absolute z-10 h-full w-full object-cover'>
@@ -15,4 +15,4 @@ const Main = () => {
 };
 
 
-export default Main
+export default test
