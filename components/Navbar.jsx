dd00f8c18ced2d8ad61356ee89e18afc81383eaa
Milk-Equity <93746647+Milk-Equity@users.noreply.github.com>
2022-09-05 20:09:46 -0400

    nav

---
+++ components/Navbar.jsx
@@ -0,0 +1,54 @@
+import React, {useState} from 'react'
+import Link from 'next/link'
+import {TbMilk} from "react-icons/tb"
+
+
+const Navbar = () => {
+    const [nav, setNav] = useState(false)
+    
+    const handleNav = () => {
+        setNav(!nav)
+    }
+
+  return (
+    <div className='fixed z-40 top-0 left-0 w-full ease-in duration-300'>
+        <div className='m-auto px-20 pt-8 flex justify-between items-center p-4 text-black'>
+            <Link href='/'>
+            <h1></h1>
+            </Link>
+            <div onClick={handleNav} className='z-10'>
+                {nav ? <TbMilk style={{fill:'white', transform: 'rotate(270deg)'}} size={50}/> : <TbMilk style={{fill:'white', }} size={50}/>}
+            </div>
+            <div className= {
+                nav 
+                ? 'absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen text-left ease-in duration-300 text-bold bg-white text-black'
+                : 'absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen text-left ease-in duration-300 text-bold bg-white text-black'
+                } 
+                >
+                <div>
+                    <div>
+                        Hello
+                    </div>
+                    <ul>
+                        <li onClick={handleNav} className='p-4 text-4xl text-bold hover:text-gray-500'>
+                            <Link href='/'>Home</Link>
+                        </li>
+                        <li onClick={handleNav} className='p-4 text-4xl hover:text-gray-500'>
+                            <Link href='/#About'>About</Link>
+                        </li>
+                        <li onClick={handleNav} className='p-4 text-4xl hover:text-gray-500'>
+                            <Link href='/#Investments'>Investments</Link>
+                        </li>
+                        <li onClick={handleNav} className='p-4 text-4xl hover:text-gray-500'>
+                            <Link href='/Diligence'>Diligence</Link>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Main from '../components/Main'
 import Head from 'next/head'
+import About from '../components/About'
 
 export default function Home() {
   return (
@@ -9,7 +10,8 @@ export default function Home() {
         <meta name="description" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-        <Main/>
+      <Main/>
+      <About/>
     </div>
   )
 }
