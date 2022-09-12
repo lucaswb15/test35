b307b36122fc6da4990daa7b5f3e315796515710
Milk-Equity <milkequity@gmail.com>
2022-09-12 14:51:10 -0400

    svg

--- components/About.jsx
+++ components/About.jsx
@@ -6,9 +6,8 @@ const About = () => {
     <div className='w-full h-screen bg-color_bg'>
       <div className='text-center'>
         <div className='columns-2 pt-52'>
-          <div className='w-[200px] h-[200px] bg-black rounded-full flex text-4xl sm:text-6xl text-white '>
-            <p>6 Entrances</p>
-          </div>
+        <div className='text-4xl sm:text-6xl text-white'>6 Entrances</div>
+
           <div className='text-4xl sm:text-6xl text-white'>Data driven investments</div>
         </div>
         <div className='columns-2 pt-24'>
@@ -17,8 +16,11 @@ const About = () => {
 
         </div>
       </div>
-      <div>
-        <Kiwi></Kiwi>
+      <div className='flex justify-center'>
+        <Kiwi><p className='text-white text-6xl'>test</p></Kiwi>
+        <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+  <circle cx="50" cy="50" r="50"/>
+</svg>
       </div>
     </div>
   )
