b852f8d59d4ee9c1023869090eacfd5cd303984b
Milk-Equity <93746647+Milk-Equity@users.noreply.github.com>
2022-09-14 21:24:32 -0400

    svg test

--- components/About.jsx
+++ components/About.jsx
@@ -18,6 +18,13 @@ const About = () => {
                 <text x="0" y="50" font-family="Verdana" font-size="35" fill="blue">Hello</text>
               </g>
             </svg>
+            <svg width="351" height="348" preserveAspectRatio='xMidYMax meet' fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M195.5 0.5C220.667 2.83333 280.1 20.1 316.5 70.5C352.9 120.9 352.667 174.833 348 195.5C346.333 223.333 327.3 287.8 264.5 323C201.7 358.2 142.333 347.667 120.5 338C95.8334 332.5 39.7 304.1 12.5 234.5C-14.7 164.9 13.8334 99.5 31.5 75.5C48.6667 51 98.6 1.7 161 0.5" stroke="white"/>
+              <text x="128" y="170" font-family="Verdana" font-size="35" fill="white">Hello</text>
+              <p x="0" y="50" font-family="Verdana" font-size="35" fill="blue">Hello</p>
+            </svg>
+
+
           </div>
 
         </div>
