a3257e91e95c539e5c5a9bebe5c9e2dfc31f4fe6
Milk-Equity <93746647+Milk-Equity@users.noreply.github.com>
2022-09-16 20:55:26 -0400

    about clean

--- components/About_live.jsx
+++ components/About_live.jsx
@@ -43,29 +43,16 @@ const About_live = () => {
   }
 
   return (
-    <div className='w-full h-screen bg-color_bg'>
-      <div className='text-center'>
-        <div className='columns-2 pt-52'>
-        <div className='text-4xl sm:text-6xl text-black'>6 Entrances</div>
-
-          <div className='text-4xl sm:text-6xl text-black'>Data driven investments</div>
-        </div>
-        <div className='columns-2 pt-24'>
-          <div className='text-4xl sm:text-6xl text-black'>$800+ AUM</div>
-          <div ref={ref} className='flex relative justify-center inset-0'>
-              <motion.svg width="410" height="410"  fill="none" xmlns="http://www.w3.org/2000/svg"
-              variants={svgVariants}
-              initial="hidden"
-              animate={controls}>
-              <motion.circle cx="202.5" cy="202.5" r="202" stroke-width="5" stroke="black"
-               variants={pathVariants}/>
-              </motion.svg>
-
+    <div className='h-screen bg-color_bg'>
+      <div ref={ref} className='flex relative justify-center inset-0'>
+       <motion.svg width="410" height="410"  fill="none" xmlns="http://www.w3.org/2000/svg"
+        variants={svgVariants}
+        initial="hidden"
+        animate={controls}>
+         <motion.circle cx="202.5" cy="202.5" r="202" stroke-width="5" stroke="black"
+          variants={pathVariants}/>
+         </motion.svg>
             <h1 className='absolute inset-y-0 my-32 text-8xl text-black'>$800+</h1>
-
-          </div>
-
-        </div>
       </div>
     </div>
   )
